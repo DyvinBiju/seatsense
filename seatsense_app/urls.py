474b55377a46872b1dce314791b6762ba4d3fd36
DyvinBiju <dyvinbiju@gmail.com>
2026-03-08 14:47:22 +0530

    Add website template and configure static files

---
+++ seatsense_app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('404/', views.page_404, name='page_404'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('FAQ/', views.faq, name='faq'),
+    path('gallery-two/', views.gallery_two, name='gallery_two'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('homepage-two/', views.homepage_two, name='homepage_two'),
+    path('', views.index, name='index'),
+    path('news-left-sidebar/', views.news_left_sidebar, name='news_left_sidebar'),
+    path('news-right-sidebar/', views.news_right_sidebar, name='news_right_sidebar'),
+    path('news-single/', views.news_single, name='news_single'),
+    path('news/', views.news, name='news'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('single-speaker/', views.single_speaker, name='single_speaker'),
+    path('speakers/', views.speakers, name='speakers'),
+    path('sponsors/', views.sponsors, name='sponsors'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+]
--- seatsense_app/views.py
+++ seatsense_app/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
-# Create your views here.
+def page_404(request):
+    return render(request, 'seatsense_app/404.html')
+
+def about_us(request):
+    return render(request, 'seatsense_app/about-us.html')
+
+def contact(request):
+    return render(request, 'seatsense_app/contact.html')
+
+def faq(request):
+    return render(request, 'seatsense_app/FAQ.html')
+
+def gallery_two(request):
+    return render(request, 'seatsense_app/gallery-two.html')
+
+def gallery(request):
+    return render(request, 'seatsense_app/gallery.html')
+
+def homepage_two(request):
+    return render(request, 'seatsense_app/homepage-two.html')
+
+def index(request):
+    return render(request, 'seatsense_app/index.html')
+
+def news_left_sidebar(request):
+    return render(request, 'seatsense_app/news-left-sidebar.html')
+
+def news_right_sidebar(request):
+    return render(request, 'seatsense_app/news-right-sidebar.html')
+
+def news_single(request):
+    return render(request, 'seatsense_app/news-single.html')
+
+def news(request):
+    return render(request, 'seatsense_app/news.html')
+
+def pricing(request):
+    return render(request, 'seatsense_app/pricing.html')
+
+def schedule(request):
+    return render(request, 'seatsense_app/schedule.html')
+
+def single_speaker(request):
+    return render(request, 'seatsense_app/single-speaker.html')
+
+def speakers(request):
+    return render(request, 'seatsense_app/speakers.html')
+
+def sponsors(request):
+    return render(request, 'seatsense_app/sponsors.html')
+
+def testimonial(request):
+    return render(request, 'seatsense_app/testimonial.html')
+
